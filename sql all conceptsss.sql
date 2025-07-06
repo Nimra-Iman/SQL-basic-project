9f10f64fd11f6feec1f94fd18697b15eed75ef31
Nimra-Iman <nimraamughal5@gmail.com>
2025-07-06 21:58:08 +0500

    add more info

--- sql all conceptsss.sql
+++ sql all conceptsss.sql
@@ -20,7 +20,7 @@ INSERT INTO emp (e_id, e_name, dept, salary) VALUES
 	(5, 'asad', "MRKT", 15000)
 ;
 
--- IF you want to add a new column
+-- =>IF you want to add a new column
 -- then you cannot directly insert values to it because y kesy pta lgy ga k 
 -- is newly-made column ki value dusry columns k kis value k corresponding enter ho gi, 
 -- for example: look below: 
@@ -29,7 +29,7 @@ insert into emp(age) values (2),(3),(4); -- this line generate error because usy
 -- nhi pta k kis e_name pr y value jay gi, e_id m issue nhi ay ga q k 
 -- us ki value auto-increment ho rhi h , ab is ko esy solve kr skty 
 -- hn k:
--- --- insert ki bijay update kr do k jha jha bhi name ki, dept or salary
+-- => insert ki bijay update kr do k jha jha bhi name ki, dept or salary
 -- ki value y h vha vha age m y value fill kr do 
 --  agr yhan name notnull na hota to
 -- ap insert kr skty age m values ko update_set use kiye bagher or vo is trah
@@ -151,11 +151,11 @@ from test_data order by new_id desc;
 select new_id, new_cat , avg(new_id) 
 over(partition by new_cat order by new_id) as sum
 from test_data ;
--- --- 1st it will partition (mean make mini tables), then sort data based on
+-- => 1st it will partition (mean make mini tables), then sort data based on
 -- order and then apply fun()
--- ---  with order by: CUMULATIVE FUNCTION
--- --- without order by, just partition by: TOTAL COUNT/AVG/SUM ETC
--- ---  min, max, count, avg, sum  ARE AGGREGATE FUNCTIONS
+-- => with order by: CUMULATIVE FUNCTION
+-- => without order by, just partition by: TOTAL COUNT/AVG/SUM ETC
+-- => min, max, count, avg, sum  ARE AGGREGATE FUNCTIONS
 
 -- -----------  rank functions  ----
 -- IN ALL RANK FUNCTIONS, OVER() K ANDR ORDER BY LIKHNA ZRURI H VRNA 
@@ -269,8 +269,4 @@ select new_cat, new_id, last_value(new_id)
 over(partition by new_cat order by new_id 
 ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as true_last_val
 from test_data;
-
-
-
-
 
